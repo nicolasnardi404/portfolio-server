1c05cdb07fb521683d8639a35c8c4273336d9719
esquizo ciborgue <nicolasnardi404@gmail.com>
2024-09-08 19:49:18 +0200

    add prisma and set up db

---
+++ server.js
@@ -0,0 +1,19 @@
+const express = require("express");
+const cors = require("cors");
+const { PrismaClient } = require("@prisma/client");
+require("dotenv").config();
+
+const app = express();
+const prisma = new PrismaClient();
+const port = process.env.PORT || 5000;
+
+app.use(cors());
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send("Hello from the backend!");
+});
+
+app.listen(port, () => {
+  console.log(`Server is running on port: ${port}`);
+});
